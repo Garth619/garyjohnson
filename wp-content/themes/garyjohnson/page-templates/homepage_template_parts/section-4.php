f87484a001c111a722154733a4bf02a3b1d08645
Garrett Cullen <Garth619@users.noreply.github.com>
2019-07-02 16:40:57 -0700

    push

---
+++ wp-content/themes/garyjohnson/page-templates/homepage_template_parts/section-4.php
@@ -0,0 +1,123 @@
+<section id="section_four">
+	
+	<div class="sec_four_inner">
+		
+		<div class="sec_four_slider">
+		
+		<div class="sec_four_slide">
+			
+			<div class="sec_four_reg">
+				
+				<div class="sec_four_reg_inner">
+					
+					<div class="sec_four_svg one">
+					
+						<?php echo file_get_contents("wp-content/themes/garyjohnson/images/sp_icon-01.svg"); ?>
+					
+					</div><!-- sec_four_svg -->
+					
+					<span class="sp_amount">$270M</span><!-- sp_amount -->
+					
+					<span class="sp_type">Gas Explosion</span><!-- sp_type -->
+					
+				</div><!-- sec_four_reg_inner -->
+				
+			</div><!-- sec_four_reg -->
+			
+			<div class="sec_four_hover">
+				
+				<div class="sec_four_hover_inner"></div><!-- sec_four_hover_inner -->
+				
+			</div><!-- sec_four_hover -->
+			
+		</div><!-- sec_four_slide -->
+		
+		<div class="sec_four_slide">
+			
+			<div class="sec_four_reg">
+				
+				<div class="sec_four_reg_inner">
+					
+					<div class="sec_four_svg two">
+					
+						<?php echo file_get_contents("wp-content/themes/garyjohnson/images/sp_icon-02.svg"); ?>
+					
+					</div><!-- sec_four_svg -->
+					
+					<span class="sp_amount">$270M</span><!-- sp_amount -->
+					
+					<span class="sp_type">Gas Explosion</span><!-- sp_type -->
+					
+				</div><!-- sec_four_reg_inner -->
+				
+			</div><!-- sec_four_reg -->
+			
+			<div class="sec_four_hover">
+				
+				<div class="sec_four_hover_inner"></div><!-- sec_four_hover_inner -->
+				
+			</div><!-- sec_four_hover -->
+			
+		</div><!-- sec_four_slide -->
+		
+		<div class="sec_four_slide">
+			
+			<div class="sec_four_reg">
+				
+				<div class="sec_four_reg_inner">
+					
+					<div class="sec_four_svg three">
+					
+						<?php echo file_get_contents("wp-content/themes/garyjohnson/images/sp_icon-03.svg"); ?>
+					
+					</div><!-- sec_four_svg -->
+					
+					<span class="sp_amount">$270M</span><!-- sp_amount -->
+					
+					<span class="sp_type">Gas Explosion</span><!-- sp_type -->
+					
+				</div><!-- sec_four_reg_inner -->
+				
+			</div><!-- sec_four_reg -->
+			
+			<div class="sec_four_hover">
+				
+				<div class="sec_four_hover_inner"></div><!-- sec_four_hover_inner -->
+				
+			</div><!-- sec_four_hover -->
+			
+		</div><!-- sec_four_slide -->
+		
+		<div class="sec_four_slide">
+			
+			<div class="sec_four_reg">
+				
+				<div class="sec_four_reg_inner">
+					
+					<div class="sec_four_svg four">
+					
+						<?php echo file_get_contents("wp-content/themes/garyjohnson/images/sp_icon-04.svg"); ?>
+					
+					</div><!-- sec_four_svg -->
+					
+					<span class="sp_amount">$270M</span><!-- sp_amount -->
+					
+					<span class="sp_type">Gas Explosion</span><!-- sp_type -->
+					
+				</div><!-- sec_four_reg_inner -->
+				
+			</div><!-- sec_four_reg -->
+			
+			<div class="sec_four_hover">
+				
+				<div class="sec_four_hover_inner"></div><!-- sec_four_hover_inner -->
+				
+			</div><!-- sec_four_hover -->
+			
+		</div><!-- sec_four_slide -->
+		
+		</div><!-- sec_four_slider -->
+		
+	</div><!-- sec_four_inner -->
+	
+</section><!-- section_four -->
