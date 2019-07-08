74caccf6775690901c9d7c091661ee6a9b83af91
Garrett Cullen <Garth619@users.noreply.github.com>
2019-07-07 19:32:34 -0700

    push

--- wp-content/themes/garyjohnson/page-templates/homepage_template_parts/section-5.php
+++ wp-content/themes/garyjohnson/page-templates/homepage_template_parts/section-5.php
@@ -1,5 +1,29 @@
 <section id="section_five">
 	
-	
+	<div class="sec_five_inner">
+		
+		<h2 class="sec_five_inner_header">lorem ipsum dol sit ametconsec</h2><!-- sec_five_inner -->
+		
+		<div class="sec_five_container">
+			
+			<div class="sec_five_image">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/content02_img.jpg"/>
+				
+			</div><!-- sec_five_image -->
+			
+			<div class="sec_five_content content">
+				
+				<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam a volutpat enim. Aenean sagittis rhoncus felis, pulvinar vulputate nunc dapibusa.</h2>
+				
+				<p>Duis ligula velit, cursus vitae varius quis, rutrum eget nibh. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam accumsan convallis lorem in rhoncus. Praesent at vulputate lorem. Morbi et elementum velit. Aenean porttitor euismod est, ut sollicitudin enim. Duis pharetra velit vitae odio volutpat pellentesque. Donec faucibus porta varius. Nunc sit amet laoreet velit. Nam urna dui, ornare ut venenatis vulputate, pellentesque aliquam nibh. Sed et ligula at nibh interdum sodales. Maecenas porttitor consectetur justo, vitae aliquet turpis vestibulum eget.</p>
+
+				<p>Duis ligula velit, cursus vitae varius quis, rutrum eget nibh. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam accumsan convallis lorem in rhoncus. Praesent at vulputate lorem. Morbi et elementum velit. Aenean porttitor euismod est, ut sollicitudin enim. Duis pharetra velit vitae odio volutpat pellentesque. Donec faucibus porta varius. Nunc sit amet laoreet velit. Nam urna dui</p>
+				
+			</div><!-- sec_five_content -->
+			
+		</div><!-- sec_five_container -->
+		
+	</div><!-- sec_five_inner -->
 	
 </section><!-- section_five -->
