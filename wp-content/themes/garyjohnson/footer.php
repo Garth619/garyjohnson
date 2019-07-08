efaf8bfce4d7fe747081fc5850b15c70316377b3
Garrett Cullen <Garth619@users.noreply.github.com>
2019-07-07 22:30:34 -0700

    push

--- wp-content/themes/garyjohnson/footer.php
+++ wp-content/themes/garyjohnson/footer.php
@@ -34,13 +34,69 @@
 			
 			<div class="footer_location_inner">
 				
-				<div class="location_col"></div><!-- location_col -->
+				<div class="location_col">
+					
+					<span class="location_col_title">Pikeville</span><!-- location_col_title -->
+					
+					<span class="street_address">110 Caroline Ave,<br/> Pikeville, KY 41501</span><!-- street_address -->
+					
+					<span class="po_box_address">P.O. Box 231<br/> Pikeville, KY 41501</span><!-- po_box_address -->
+					
+					<span class="phone_title">Phone</span><!-- phone_title -->
+					
+					<a class="footer_phone" href="">(606) 437-40021</a><!-- footer_phone -->
+					
+					<a class="get_directions" href="">Get Directions</a><!-- get_directions -->
+					
+				</div><!-- location_col -->
 				
-				<div class="location_col"></div><!-- location_col -->
+				<div class="location_col">
+					
+					<span class="location_col_title">Hazard</span><!-- location_col_title -->
+					
+					<span class="street_address">941 N Main St #202,<br/> Hazard, KY 41701</span><!-- street_address -->
+					
+					<span class="po_box_address">P.O. Box 359<br/> Hazard, KY 41701</span><!-- po_box_address -->
+					
+					<span class="phone_title">Phone</span><!-- phone_title -->
+					
+					<a class="footer_phone" href="">(606) 436-6059</a><!-- footer_phone -->
+					
+					<a class="get_directions" href="">Get Directions</a><!-- get_directions -->
+					
+				</div><!-- location_col -->
 				
-				<div class="location_col"></div><!-- location_col -->
+				<div class="location_col">
+					
+					<span class="location_col_title">Lexington</span><!-- location_col_title -->
+					
+					<span class="street_address">2265 Executive Dr,<br/> Lexington, KY 40505</span><!-- street_address -->
+					
+					<span class="po_box_address">P.O. Box 55106<br/> Lexington, KY 40505</span><!-- po_box_address -->
+					
+					<span class="phone_title">Phone</span><!-- phone_title -->
+					
+					<a class="footer_phone" href="">(606) 268-4300</a><!-- footer_phone -->
+					
+					<a class="get_directions" href="">Get Directions</a><!-- get_directions -->
+					
+				</div><!-- location_col -->
 				
-				<div class="location_col"></div><!-- location_col -->
+				<div class="location_col">
+					
+					<span class="location_col_title">Louisville</span><!-- location_col_title -->
+					
+					<span class="street_address">9300 Shelbyville Rd,<br/> Ste. 609<br/> Louisville, KY 40222</span><!-- street_address -->
+					
+					<span class="phone_title">Phone</span><!-- phone_title -->
+					
+					<a class="footer_phone" href="">(606) 437-40021</a><!-- footer_phone -->
+					
+					<a class="get_directions" href="">Get Directions</a><!-- get_directions -->
+					
+				</div><!-- location_col -->
+				
+				<a class="locations_button" href="">View All Locations</a><!-- locations_button -->
 				
 			</div><!-- footer_location_inner -->
 			
