4b500314d2fc2647b0061504a5191e57b03a3d99
Garrett Cullen <Garth619@users.noreply.github.com>
2019-07-10 20:20:37 -0700

    push

--- wp-content/themes/garyjohnson/page-templates/template-banner.php
+++ wp-content/themes/garyjohnson/page-templates/template-banner.php
@@ -7,6 +7,31 @@
 			<?php if(is_category()) : ?>
 			
 				<span class="banner_header"><?php single_cat_title();?></span>
+				
+			<?php elseif(is_archive()) : ?>
+			
+					<?php if ( have_posts() )the_post();?>
+
+					<span class="banner_header">
+					
+						<?php if ( is_day() ) : 
+							
+								printf( __( '<span>%s</span>', 'twentyten' ), get_the_date() ); 
+						
+							elseif ( is_month() ) : 
+							
+								printf( __( '<span>%s</span>', 'twentyten' ), get_the_date( _x( 'F Y', 'monthly archives date format', 'twentyten' ) ) );
+						
+							elseif ( is_year() ) :
+						
+								printf( __( '<span>%s</span>', 'twentyten' ), get_the_date( _x( 'Y', 'yearly archives date format', 'twentyten' ) ) );
+						
+						endif; ?>
+					
+					</span><!-- banner_header -->
+			
+		<?php rewind_posts(); ?>
+
 			
 			<?php else: ?>
 			
