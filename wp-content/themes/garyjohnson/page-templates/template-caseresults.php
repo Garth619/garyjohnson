9ca99b45165b44094dc4409b600aa5a95e5594cd
Garrett Cullen <Garth619@users.noreply.github.com>
2019-07-11 11:13:35 -0700

    push

--- wp-content/themes/garyjohnson/page-templates/template-caseresults.php
+++ wp-content/themes/garyjohnson/page-templates/template-caseresults.php
@@ -5,7 +5,145 @@
 get_header(); ?>
 
 
+<div class="internal_main">
+
+	<div class="internal_container internal_wrapper">
+	
+		<h1 class="internal_header"><?php the_title();?></h1><!-- internal_header -->
+	
+		<div class="case_results_wrapper">
+	
+			<span class="case_results_intro">Many of the following cases were settled after trial for amounts less than the actual verdict listed below, in some cases, substantially less. The reasons for settlement after a trial verdict are many, including the length of time a case might be on appeal, the likelihood of losing on appeal, limitations in insurance coverage and sometimes a client's desire to simply put their traumatic and painful experience behind them.</span><!-- case_results_intro -->
+			
+			<div class="case_results_grid">
+				
+				<div class="cr_col">
+					
+					<div class="cr_single">
 						
+						<span class="cr_amount">$270,050,323.57</span>
+						
+						<span class="cr_type">Gas Explosion</span><!-- cr_type -->
+						
+						<div class="cr_content_wrapper content">
+							
+							<div class="facts">
+							
+								<span class="cr_subtitle">Facts</span><!-- cr_subtitle -->
+								
+								<p>Plaintiff was in his pump house when a gas explosion erupted from his water well, causing plaintiff to suffer burns to his face and arm area. Suit was filed against the gas company for drilling wells in a manner that allowed natural gas to escape into the water table, thereby causing the explosion. During trial, it was revealed that not only was the gas well next to plaintiff's house drilled this way, but also gas wells all over eastern Kentucky.</p>
+							
+							</div><!-- facts -->
+							
+							<div class="results">
+							
+								<span class="cr_subtitle">Result</span><!-- cr_subtitle -->
+								
+								<p>Trial resulted in a verdict of $20,050,323.57 in compensatory damages and $250,000,000 in punitive damages. It is believed to be the largest single plaintiff verdict in the Commonwealth of Kentucky. The case was subsequently settled prior to appeal for a confidential amount substantially less than the verdict.</p>
+							
+							</div><!-- facts -->
+							
+						</div><!-- cr_content_wrapper -->
+						
+					</div><!-- cr_single -->
+					
+					<div class="cr_single">
+						
+						<span class="cr_amount">$270,050,323.57</span>
+						
+						<span class="cr_type">Gas Explosion</span><!-- cr_type -->
+						
+						<div class="cr_content_wrapper content">
+							
+							<div class="facts">
+							
+								<span class="cr_subtitle">Facts</span><!-- cr_subtitle -->
+								
+								<p>Plaintiff was in his pump house when a gas explosion erupted from his water well, causing plaintiff to suffer burns to his face and arm area. Suit was filed against the gas company for drilling wells in a manner that allowed natural gas to escape into the water table, thereby causing the explosion. During trial, it was revealed that not only was the gas well next to plaintiff's house drilled this way, but also gas wells all over eastern Kentucky.</p>
+							
+							</div><!-- facts -->
+							
+							<div class="results">
+							
+								<span class="cr_subtitle">Result</span><!-- cr_subtitle -->
+								
+								<p>Trial resulted in a verdict of $20,050,323.57 in compensatory damages and $250,000,000 in punitive damages. It is believed to be the largest single plaintiff verdict in the Commonwealth of Kentucky. The case was subsequently settled prior to appeal for a confidential amount substantially less than the verdict.</p>
+							
+							</div><!-- facts -->
+							
+						</div><!-- cr_content_wrapper -->
+						
+					</div><!-- cr_single -->
+					
+				</div><!-- cr_col -->
+				
+				<div class="cr_col">
+					
+					<div class="cr_single">
+						
+						<span class="cr_amount">$270,050,323.57</span>
+						
+						<span class="cr_type">Gas Explosion</span><!-- cr_type -->
+						
+						<div class="cr_content_wrapper content">
+							
+							<div class="facts">
+							
+								<span class="cr_subtitle">Facts</span><!-- cr_subtitle -->
+								
+								<p>Plaintiff was in his pump house when a gas explosion erupted from his water well, causing plaintiff to suffer burns to his face and arm area. Suit was filed against the gas company for drilling wells in a manner that allowed natural gas to escape into the water table, thereby causing the explosion. During trial, it was revealed that not only was the gas well next to plaintiff's house drilled this way, but also gas wells all over eastern Kentucky.</p>
+							
+							</div><!-- facts -->
+							
+							<div class="results">
+							
+								<span class="cr_subtitle">Result</span><!-- cr_subtitle -->
+								
+								<p>Trial resulted in a verdict of $20,050,323.57 in compensatory damages and $250,000,000 in punitive damages. It is believed to be the largest single plaintiff verdict in the Commonwealth of Kentucky. The case was subsequently settled prior to appeal for a confidential amount substantially less than the verdict.</p>
+							
+							</div><!-- facts -->
+							
+						</div><!-- cr_content_wrapper -->
+						
+					</div><!-- cr_single -->
+					
+					<div class="cr_single">
+						
+						<span class="cr_amount">$270,050,323.57</span>
+						
+						<span class="cr_type">Gas Explosion</span><!-- cr_type -->
+						
+						<div class="cr_content_wrapper content">
+							
+							<div class="facts">
+							
+								<span class="cr_subtitle">Facts</span><!-- cr_subtitle -->
+								
+								<p>Plaintiff was in his pump house when a gas explosion erupted from his water well, causing plaintiff to suffer burns to his face and arm area. Suit was filed against the gas company for drilling wells in a manner that allowed natural gas to escape into the water table, thereby causing the explosion. During trial, it was revealed that not only was the gas well next to plaintiff's house drilled this way, but also gas wells all over eastern Kentucky.</p>
+							
+							</div><!-- facts -->
+							
+							<div class="results">
+							
+								<span class="cr_subtitle">Result</span><!-- cr_subtitle -->
+								
+								<p>Trial resulted in a verdict of $20,050,323.57 in compensatory damages and $250,000,000 in punitive damages. It is believed to be the largest single plaintiff verdict in the Commonwealth of Kentucky. The case was subsequently settled prior to appeal for a confidential amount substantially less than the verdict.</p>
+							
+							</div><!-- facts -->
+							
+						</div><!-- cr_content_wrapper -->
+						
+					</div><!-- cr_single -->
+					
+				</div><!-- cr_col -->
+				
+			</div><!-- case_results_grid -->
+	
+		</div><!-- case_results_wrapper -->
+	
+	</div><!-- internal_container -->
+
+</div><!-- internal_main -->					
 			  
 
 
