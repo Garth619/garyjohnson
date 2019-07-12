89bbbf1eb8e36701dfde5e0924e6eaced76e6d46
Garrett Cullen <Garth619@users.noreply.github.com>
2019-07-11 17:18:52 -0700

    push

--- wp-content/themes/garyjohnson/page-templates/template-contact.php
+++ wp-content/themes/garyjohnson/page-templates/template-contact.php
@@ -5,6 +5,17 @@
 	get_header(); ?>
 
 
+<div class="internal_main">
+
+<div class="internal_container internal_wrapper" style="background:url(<?php bloginfo('template_directory');?>/images/light-grey-terrazzo.png) top left repeat">
+	
+	<h1 class="internal_header"><?php the_title();?></h1><!-- internal_header -->
+	
+		
+</div><!-- internal_container -->
+
+</div><!-- internal_main -->
+
 
 			
 <?php get_footer(); ?>
