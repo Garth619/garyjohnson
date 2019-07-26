30ae7b17b36bf06ac5530920f0a3942ff8a7a4de
Garrett Cullen <Garth619@users.noreply.github.com>
2019-07-26 16:42:31 -0700

    push

--- wp-content/themes/garyjohnson/page-templates/template-testimonials.php
+++ wp-content/themes/garyjohnson/page-templates/template-testimonials.php
@@ -11,7 +11,99 @@ get_header(); ?>
 	
 	<h1 class="internal_header"><?php the_title();?></h1><!-- internal_header -->
 	
-	
+	<div class="testimonial_wrapper">
+		
+		<div class="testi_col">
+			
+			<div class="single_testl">
+				
+				<div class="single_test_content">
+					
+					<p>“This has been my second time working with a lawyer firm and let me tell you they were the best!! I was very well taken care of. My case was handled with such grace. I highly recommend them. They were friendly with all my worries.”</p>
+					
+					<span class="testi_name">Cassie Edmiston</span><!-- testi_name -->
+					
+				</div><!-- single_test_content -->
+				
+			</div><!-- single_testl -->
+			
+			<div class="single_testl">
+				
+				<div class="single_test_content">
+					
+					<p>“This has been my second time working with a lawyer firm and let me tell you they were the best!! I was very well taken care of. My case was handled with such grace. I highly recommend them. They were friendly with all my worries.”</p>
+					
+					<p>“This has been my second time working with a lawyer firm and let me tell you they were the best!! I was very well taken care of. My case was handled with such grace. I highly recommend them. They were friendly with all my worries.”</p>
+					
+					<span class="testi_name">Cassie Edmiston</span><!-- testi_name -->
+					
+				</div><!-- single_test_content -->
+				
+			</div><!-- single_testl -->
+			
+			<div class="single_testl">
+				
+				<div class="single_test_content">
+					
+					<p>“This has been my second time working with a lawyer firm and let me tell you they were the best!! I was very well taken care of. My case was handled with such grace. I highly recommend them. They were friendly with all my worries.”</p>
+					
+					<p>“This has been my second time working with a lawyer firm and let me tell you they were the best!! I was very well taken care of. My case was handled with such grace. I highly recommend them. They were friendly with all my worries.”</p>
+					
+					<span class="testi_name">Cassie Edmiston</span><!-- testi_name -->
+					
+				</div><!-- single_test_content -->
+				
+			</div><!-- single_testl -->
+			
+		</div><!-- testi_col -->
+		
+		<div class="testi_col">
+			
+			<div class="single_testl">
+				
+				<div class="single_test_content">
+					
+					<p>“This has been my second time working with a lawyer firm and let me tell you they were the best!! I was very well taken care of. My case was handled with such grace. I highly recommend them. They were friendly with all my worries.”</p>
+					
+					<p>“This has been my second time working with a lawyer firm and let me tell you they were the best!! I was very well taken care of. My case was handled with such grace. I highly recommend them. They were friendly with all my worries.”</p>
+					
+					<span class="testi_name">Cassie Edmiston</span><!-- testi_name -->
+					
+				</div><!-- single_test_content -->
+				
+			</div><!-- single_testl -->
+			
+			<div class="single_testl">
+				
+				<div class="single_test_content">
+					
+					<p>“This has been my second time working with a lawyer firm and let me tell you they were the best!! I was very well taken care of. My case was handled with such grace. I highly recommend them. They were friendly with all my worries.”</p>
+					
+					<p>“This has been my second time working with a lawyer firm and let me tell you they were the best!! I was very well taken care of. My case was handled with such grace. I highly recommend them. They were friendly with all my worries.”</p>
+					
+					<span class="testi_name">Cassie Edmiston</span><!-- testi_name -->
+					
+				</div><!-- single_test_content -->
+				
+			</div><!-- single_testl -->
+			
+			<div class="single_testl">
+				
+				<div class="single_test_content">
+					
+					<p>“This has been my second time working with a lawyer firm and let me tell you they were the best!! I was very well taken care of. My case was handled with such grace. I highly recommend them. They were friendly with all my worries.”</p>
+					
+					<p>“This has been my second time working with a lawyer firm and let me tell you they were the best!! I was very well taken care of. My case was handled with such grace. I highly recommend them. They were friendly with all my worries.”</p>
+					
+					<span class="testi_name">Cassie Edmiston</span><!-- testi_name -->
+					
+				</div><!-- single_test_content -->
+				
+			</div><!-- single_testl -->
+			
+		</div><!-- testi_col -->
+		
+	</div><!-- testimonial_wrapper -->
 	
 </div><!-- internal_container -->
 
